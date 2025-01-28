b628861ba48ed19059ec4eff7cce7ee76bd7d361
Amna <amnaarifkeepup@gmail.com>
2025-01-28 15:37:10 +0500

    first

---
+++ lib/const_color.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Blues
+  static const Color primaryBlue = Color(0xFF0D47A1);
+  static const Color secondaryBlue = Color(0xFF1976D2);
+  static const Color lightBlue = Color(0xFFBBDEFB);
+  static const Color skyBlue = Color(0xFF81D4FA);
+  static const Color aquaBlue = Color(0xFF4FC3F7);
+
+
+  // Whites
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color offWhite = Color(0xFFF5F5F5);
+  static const Color creamWhite = Color(0xFFFFFAFA);
+
+  // Additional Colors
+  static const Color lightGray = Color(0xFFBDBDBD);
+  static const Color accentBlue = Color(0xFF1E88E5);
+}
